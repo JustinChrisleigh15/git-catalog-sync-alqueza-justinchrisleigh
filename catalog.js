50a9f2b3df810d93f64b05caf78f2b76784ec911
JustinChrisleigh15 <justinchrisleigh.alqueza@cit.edu>
2026-09-23 19:39:35 +0800

    Remove isValidLoan function and adjust exports
    Removed isValidLoan function and updated exports.

--- catalog.js
+++ catalog.js
@@ -1,9 +1,6 @@
-function isValidLoan(daysLate) {
-  return daysLate >= 0;
-}
-
 function calculateLateFee(daysLate, ratePerDay) {
+  if (daysLate <= 1) {
+    return 0;
+  }
   return Math.floor(daysLate * ratePerDay);
 }
-
-module.exports = { isValidLoan, calculateLateFee };
